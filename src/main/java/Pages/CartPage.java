0d0c4911747f27857995b3e3d4ed8ca6f93aa82b
BRC1234210 <askbarrcan41@gmail.com>
2025-11-15 01:03:24 +0300

    Merge yapıldıf

--- src/main/java/Pages/CartPage.java
+++ src/main/java/Pages/CartPage.java
@@ -6,13 +6,21 @@ import org.openqa.selenium.support.FindBy;
 
 public class CartPage extends BasePage{
 
-    @FindBy(xpath = "//button[@class='Payment-Button CC']")
-    private WebElement payUsingDebitOrCreditCartButton;
+    @FindBy(css = "#Overlay > div > div.container.Cart > div > div.column.one-half.RightColumn > div.Payment-Section > div > button.Payment-Button.CC")
+    public WebElement payUsingDebitOrCreditCartButton;
 
     public CartPage(WebDriver driver) {
         super(driver);
     }
     public void clickPayUsingDebitOrCreditCartButton(){
         clickElement(payUsingDebitOrCreditCartButton);
+    }
+
+
+    @FindBy(xpath = "//button[@class='Pay-Button']")
+    public WebElement payButton;     // ödeme yap butonu
+
+    public void clickPayButton(){
+        clickElement(payButton);
     }
 }
